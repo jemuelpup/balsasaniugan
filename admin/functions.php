878f0ba2df0cfbeddeee790b2fae1e16fc8cb6a4
jemuelpup <jemuelpup@gmail.com>
2018-01-30 17:48:55 +0800

    admin panel ongoing

--- admin/functions.php
+++ admin/functions.php
@@ -29,28 +29,69 @@ else{
 }
 
 switch($process){
-	case "FunctionName":{
-		functionName($conn,$data);
-	}
+	case "AddEmployee":{insertEmployee($conn,$data);}break;
+	case "EditEmployee":{updateEmployee($conn,$data);}break;
+	case "AddPosition":{insertPosition($conn,$data);}break;
+	case "EditPosition":{updatePosition($conn,$data);}break;
+	case "AddProduct":{insertProduct($conn,$data);}break;
+	case "EditProduct":{updateProduct($conn,$data);}break;
+	case "AddCategory":{insertCategory($conn,$data);}break;
+	case "EditCategory":{updateCategory($conn,$data);}break;
 }
 
 
 /* function area */
-// insert function block
-function insertFunctionName($c,$d){
-	//sample codes inside the function
-	$sql = $c->prepare("INSERT INTO category_tbl (name,category_code,description) VALUES (?,?,?)");
-	$sql->bind_param('sss',$d->name,$d->category_code,$d->description);
-	$msg = ($sql->execute() === TRUE) ? "Adding new Category success" : "Error: " . $sql . "<br>" . $c->error;
+// employee functions
+function insertEmployee($c,$d){
+
+	$sql = $c->prepare("INSERT INTO employee_tbl(name,picture,address,contact_number,email,position_fk,branch_fk,salary,modified_by_fk,birth_day,gender)VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+	$sql->bind_param('sssssiidisi',$d->name,$d->picture,$d->address,$d->contact_number,$d->email,$d->position_fk,$d->branch_fk,$d->salary,$d->modified_by_fk,$d->birth_day,$d->gender);
+	$msg = ($sql->execute() === TRUE) ? "Adding new Employee success" : "Error: " . $sql . "<br>" . $c->error;
+	$sql->close();
+	/**/
+}
+function updateEmployee($c,$d){
+	$sql = $c->prepare("UPDATE employee_tbl SET name = ?, picture = ?, address = ?, contact_number = ?, email = ?, position_fk = ?, branch_fk = ?, salary = ?, modified_by_fk = ?, birth_day = ?, gender = ?;"); 
+	$sql->bind_param('sssssiidisi',$d->name,$d->picture,$d->address,$d->contact_number,$d->email,$d->position_fk,$d->branch_fk,$d->salary,$d->modified_by_fk,$d->birth_day,$d->gender);
+	$msg = ($sql->execute() === TRUE) ? "Editting Employee success" : "Error: " . $sql . "<br>" . $c->error;
+	$sql->close();
+}
+//position functions
+function insertPosition($c,$d){
+	$sql = $c->prepare("INSERT INTO position_tbl(name,description)VALUES(?,?)");
+	$sql->bind_param('ss',$d->name,$d->description);
+	$msg = ($sql->execute() === TRUE) ? "Adding position success" : "Error: " . $sql . "<br>" . $c->error;
+	$sql->close();
+}
+function updatePosition($c,$d){
+	$sql = $c->prepare("UPDATE position_tbl SET name = ?, description = ?");
+	$sql->bind_param('ss',$d->name,$d->description);
+	$msg = ($sql->execute() === TRUE) ? "Editting Position success" : "Error: " . $sql . "<br>" . $c->error;
+	$sql->close();
+}
+function insertProduct($c,$d){
+	$sql = $c->prepare("INSERT INTO product_tbl(name,description,picture,item_code,category_fk,modified_by_fk,price)VALUES(?,?,?,?,?,?,?)");
+	$sql->bind_param('ssssiid',$d->name,$d->description,$d->picture,$d->item_code,$d->category_fk,$d->modified_by_fk,$d->price);
+	$msg = ($sql->execute() === TRUE) ? "Adding Product success" : "Error: " . $sql . "<br>" . $c->error;
+	$sql->close();
+}
+function updateProduct($c,$d){
+	$sql = $c->prepare("UPDATE product_tbl SET name = ?, description = ?, picture = ?, item_code = ?, category_fk = ?, modified_by_fk = ?, price = ?");
+	$sql->bind_param('ssssiid',$d->name,$d->description,$d->picture,$d->item_code,$d->category_fk,$d->modified_by_fk,$d->price);
+	$msg = ($sql->execute() === TRUE) ? "Editting Product success" : "Error: " . $sql . "<br>" . $c->error;
 	$sql->close();
 }
 
-// update function block
-function updateFunctionName($c,$d){
-	//sample codes inside the function
-	$sql = $c->prepare("UPDATE order_tbl SET cashier_fk = ? ,payment = ?,received_date = NOW() WHERE id = ?");
-	$sql->bind_param('idi', $_SESSION["employeeID"], validateData($d->cash), validateData($d->id));
-	$msg = ($sql->execute() === TRUE) ? "Setting order paid success" : "Error: " . $sql . "<br>" . $c->error;
+function insertCategory($c,$d){
+	$sql = $c->prepare("INSERT INTO category_tbl(name,category_code,description)VALUES(?,?,?)");
+	$sql->bind_param('sss',$d->name,$d->category_code,$d->description);
+	$msg = ($sql->execute() === TRUE) ? "Adding Category success" : "Error: " . $sql . "<br>" . $c->error;
+	$sql->close();
+}
+function updateCategory($c,$d){
+	$sql = $c->prepare("UPDATE category_tbl SET name = ?, category_code = ?, description = ?");
+	$sql->bind_param('sss',$d->name,$d->category_code,$d->description);
+	$msg = ($sql->execute() === TRUE) ? "Editting Category success" : "Error: " . $sql . "<br>" . $c->error;
 	$sql->close();
 }
 
--- admin/js/angular/controllers.js
+++ admin/js/angular/controllers.js
@@ -1,4 +1,46 @@
-app.controller("controllerName",function($scope,dbOperations){
+app.controller("employeeManagement",function($scope,dbOperations){
+
+	$scope.employeeFields = {}; //means object
+	console.log("Gumagana");
+	
+
+	$scope.addEmployee = function(){
+		dbOperations.processData("AddEmployee",$scope.employeeFields).then(function(res){
+			console.log(res);
+		});
+	}
+});
+
+app.controller("productManagement",function($scope,dbOperations){
+
+	// $scope.functionName = function(){
+	// 	dbOperations.processData("processName","").then(function(res){
+	// 		console.log(res);
+	// 	});
+	// }
+	console.log("Gumagana");
+	$scope.addProduct = function(){
+		dbOperations.processData("AddProduct",$scope.productFields).then(function(res){
+			console.log(res);
+		});
+	}
+	$scope.addCategory = function(){
+		dbOperations.processData("AddCategory",$scope.categoryFields).then(function(res){
+			console.log(res);
+		});
+	}
+});
+app.controller("buisnessManagement",function($scope,dbOperations){
+
+	$scope.addPosition = function(){
+		dbOperations.processData("AddPosition",$scope.positionFields).then(function(res){
+			console.log(res);
+		});
+		console.log($scope.positionFields);
+		// console.log("buisness");
+	}
+});
+app.controller("reports",function($scope,dbOperations){
 
 	// $scope.functionName = function(){
 	// 	dbOperations.processData("processName","").then(function(res){

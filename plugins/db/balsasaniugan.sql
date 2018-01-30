9cfe508cc0d0164000fb73a67ce4b489a42ebd73
jemuelpup <jemuelpup@gmail.com>
2018-01-30 17:43:01 +0800

    admin 15%

--- plugins/db/balsasaniugan.sql
+++ plugins/db/balsasaniugan.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2018 at 06:51 AM
+-- Generation Time: Jan 30, 2018 at 10:26 AM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `access_tbl` (
 --
 
 CREATE TABLE IF NOT EXISTS `branch_tbl` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `address` varchar(200) DEFAULT NULL,
   `description` varchar(200) DEFAULT NULL,
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `branch_tbl` (
 --
 
 CREATE TABLE IF NOT EXISTS `category_tbl` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `category_code` varchar(10) DEFAULT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `category_tbl` (
 --
 
 CREATE TABLE IF NOT EXISTS `employee_tbl` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `picture` varchar(100) DEFAULT NULL,
   `address` varchar(50) DEFAULT NULL,
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `employee_tbl` (
 --
 
 CREATE TABLE IF NOT EXISTS `ingredients_tbl` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `quantity` int(11) DEFAULT '1',
   `unit` varchar(15) DEFAULT 'pieces',
@@ -128,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `order_line_tbl` (
 --
 
 CREATE TABLE IF NOT EXISTS `order_tbl` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `order_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `seat_number` int(11) DEFAULT '0',
   `cashier_fk` int(11) NOT NULL,
@@ -140,7 +140,8 @@ CREATE TABLE IF NOT EXISTS `order_tbl` (
   `payment` decimal(11,2) DEFAULT NULL,
   `notes` varchar(200) DEFAULT NULL,
   `down_payment` decimal(11,2) NOT NULL DEFAULT '0.00',
-  `received_date` datetime DEFAULT NULL
+  `received_date` datetime DEFAULT NULL,
+  `void_reason` varchar(150) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -150,11 +151,18 @@ CREATE TABLE IF NOT EXISTS `order_tbl` (
 --
 
 CREATE TABLE IF NOT EXISTS `position_tbl` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `position_tbl`
+--
+
+INSERT INTO `position_tbl` (`id`, `name`, `description`, `active`) VALUES
+(1, 'Waiter', 'Gets the orders and the payment of the customers', 1);
 
 -- --------------------------------------------------------
 
@@ -163,8 +171,9 @@ CREATE TABLE IF NOT EXISTS `position_tbl` (
 --
 
 CREATE TABLE IF NOT EXISTS `product_tbl` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `description` varchar(300) NOT NULL DEFAULT 'no desc',
   `picture` varchar(100) DEFAULT NULL,
   `item_code` varchar(10) DEFAULT NULL,
   `category_fk` int(11) DEFAULT NULL,
@@ -172,9 +181,94 @@ CREATE TABLE IF NOT EXISTS `product_tbl` (
   `modified_by_fk` int(11) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1',
   `price` decimal(11,2) DEFAULT NULL,
-  `available` int(11) NOT NULL DEFAULT '1'
+  `amount` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branch_tbl`
+--
+ALTER TABLE `branch_tbl`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category_tbl`
+--
+ALTER TABLE `category_tbl`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee_tbl`
+--
+ALTER TABLE `employee_tbl`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ingredients_tbl`
+--
+ALTER TABLE `ingredients_tbl`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_tbl`
+--
+ALTER TABLE `order_tbl`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `position_tbl`
+--
+ALTER TABLE `position_tbl`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_tbl`
+--
+ALTER TABLE `product_tbl`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branch_tbl`
+--
+ALTER TABLE `branch_tbl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `category_tbl`
+--
+ALTER TABLE `category_tbl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `employee_tbl`
+--
+ALTER TABLE `employee_tbl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ingredients_tbl`
+--
+ALTER TABLE `ingredients_tbl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `order_tbl`
+--
+ALTER TABLE `order_tbl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `position_tbl`
+--
+ALTER TABLE `position_tbl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `product_tbl`
+--
+ALTER TABLE `product_tbl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
